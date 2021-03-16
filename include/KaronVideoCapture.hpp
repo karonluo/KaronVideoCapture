c88ba2fc00a89da86b0aa9ab41ff56d64df0bac1
Karon Luo <lk139@126.com>
2021-03-16 16:05:05 +0800

    Update KaronVideoCapture.hpp

--- include/KaronVideoCapture.hpp
+++ include/KaronVideoCapture.hpp
@@ -14,36 +14,95 @@
 
 #define MAT_BUFFER_SIZE 2
 
-/*
-Karon Video Capture
-Version: 1.0
-Author: Karon Luo
-Date: 2021-03-01
-Dependencies: vaapi\ffmpeg\opencv\live555\vlc3.0+
-*/
-
+/**
+ * @brief Karon Video Capture 用于获取视频帧数据，基于 VideoLan\OpenCV\FFMpeg\Live555 开源项目
+ * @author Karon.Luo kluo@edgevision.ai
+ * @date 2021-03-01
+ * @version 1.0
+ * @note Dependencies: vaapi\ffmpeg\opencv\live555\vlc3.0+
+ */
 class KaronVideoCapture
 {
 public:
-	/*
-		VideoUrl, example: rtsp://username:password@ip or /home/download/video.mp4
-		Width, example: 1920
-		Height, example: 1080
-	*/
+
+	/**
+	 * @brief 构造一个新的 Karon Video Capture 对象
+	 * @param VideoUrl 视频源地址 文件(video file address)或者地址（rtsp) 
+	 * @param VideoWidth 视频源宽度
+	 * @param VideoHeigth 视频源高度
+	 */
 	KaronVideoCapture(std::string VideoUrl, int VideoWidth, int VideoHeigth);
+	
+	/**
+	 * @brief 构造一个新的 Karon Video Capture 对象
+	 * 
+	 */
 	KaronVideoCapture();
+
+	
+	/**
+	 * @brief 获取视频宽度
+	 * 
+	 * @return int
+	 */
 	int GetVideoWidth();
+	
+	/**
+	 * @brief 获取视频高度
+	 * @return int
+	 */
 	int GetVideoHeight();
-	int GetVideoFps();
-	/*
-			Get OpenCV Mat object 
-			defalut retry times is 1000
+	
+	/** 
+	* @brief 获取视频源的帧数
+	* @return int
 	*/
+	int GetVideoFps();
+	
+	/**
+	 * @brief 获取视频 OpenCV Mat 对象
+	 * 
+	 * @param RetryCount 重试次数 
+	 * @return cv::Mat OpenCV Mat 对象
+	 */
 	cv::Mat GetVideoMat(int RetryCount = 1000);
+
+
+	/**
+	 * @brief 判断视频源是否正常打开
+	 * 
+	 * @param RetryCount 重试次数
+	 * @return true 
+	 * @return false 
+	 */
 	bool IsOpened(int RetryCount = 1000);
+	/**
+	 * @brief 释放对象，手动调用析构函数 
+	 * 
+	 */
 	void Release();
+	/**
+	 * @brief 重新打开视频源
+	 * 
+	 */
 	void Reopen();
+
+	/**
+	 * @brief 打开指定视频源
+	 * 
+	 * @param VideoUrl 视频源地址，可以是文件路径也可以是 RTSP 协议路径
+	 * @param VideoWidth 指定视频源宽度
+	 * @param VideoHeight 指定视频源高度
+	 * @param IsReopen 本次打开是否是重新打开
+	 * @note 如果 IsReopen 为 true, 则相当于执行 Reopen 方法, 视频源的高宽设置决定内存分配的大小一定需要根据视频源的高宽进行指定。
+	 * 
+	 */
 	void Open(std::string VideoUrl, int VideoWidth, int VideoHeight,bool IsReopen = false);
+
+	/**
+	 * @brief 析构函数，销毁 Karon Video Capture 对象
+	 * 
+	 */
 	~KaronVideoCapture();
 
 private:
